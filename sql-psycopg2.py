4322c332a39633310b85c2c0e4cc181a6813e1c0
GregWebDev <code@greggoodrem.co.uk>
2022-09-03 11:34:50 +0000

    Query searches added and tested

---
+++ sql-psycopg2.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+connection = psycopg2.connect(database="chinook")
+
+cursor = connection.cursor()
+# Query 1
+# cursor.execute('SELECT * FROM "Artist"')
+# Query 2
+# cursor.execute('SELECT "Name" FROM "Artist"')
+# Query 3
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Led Zeppelin"])
+#cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [128])
+
+
+
+results = cursor.fetchall()
+
+connection.close()
+
+for result in results:
+    print(result)
+
